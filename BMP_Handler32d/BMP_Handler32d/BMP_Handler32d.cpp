23dea27ce46197c1db95796e9199308598816ded
GarthVader91 <murraygn1@gcc.edu>
2015-10-19 16:27:13 -0400

    FINAL Final Version

--- BMP_Handler32d/BMP_Handler32d/BMP_Handler32d.cpp
+++ BMP_Handler32d/BMP_Handler32d/BMP_Handler32d.cpp
@@ -1,7 +1,7 @@
 /**********************************************************
 Program: BMP_Handler32d.cpp
 Author: Garth Murray
-Date: 10/18/2015
+Date: 10/19/2015
 Description: This file implements the functions provided by 
 the BMP_Handler.h header file, which are intended to read
 and write bits in a BMP file in order to store specific 
@@ -21,6 +21,8 @@ BITMAPINFOHEADER bmpInfo;
 BMP_API unsigned char* BMP_Handler::loadBMP(const char* filename, int& width, int& height)
 {	
 	std::ifstream file(filename, std::ifstream::binary);
+
+	//Checks if file can be opened
 	if(!file)
 	{
 		std::cout << "Failure to open bitmap file.\n";
@@ -31,10 +33,18 @@ BMP_API unsigned char* BMP_Handler::loadBMP(const char* filename, int& width, in
 	file.read((char*)&bmpHeader, sizeof(BITMAPFILEHEADER));
 	file.read((char*)&bmpInfo, sizeof(BITMAPINFOHEADER));
 
+	// Check if the file is an actual BMP file
+	if(bmpHeader.bfType != 0x4D42)
+	{
+		std::cout << "File \"" << filename << "\" isn't a bitmap file\n";
+		return NULL;
+	}
+
 	// Set width and height to the values loaded from the file
 	width = (int)bmpInfo.biWidth;
 	height = (int)bmpInfo.biHeight;
 
+	//Array that will store altered bits
 	unsigned char* pixels = new unsigned char[bmpHeader.bfSize];
 
 	//Reads in RGB values to the pixels array
@@ -48,6 +58,7 @@ BMP_API void BMP_Handler::saveBMP(const char* filename, const unsigned char* RGB
 {
 	std::ofstream myfile(filename, std::ios::out | std::ios::binary | std::ofstream::trunc);
 
+	//Moves through headers
 	myfile.write((const char*)&bmpHeader, sizeof(BITMAPFILEHEADER));
 	myfile.write((const char*)&bmpInfo, sizeof(BITMAPINFOHEADER));
 
