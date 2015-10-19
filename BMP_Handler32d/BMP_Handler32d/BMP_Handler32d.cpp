9f453a793adc840dcac504a4b3e127a38f7e5c1e
GarthVader91 <murraygn1@gcc.edu>
2015-10-19 19:19:36 -0400

    Padding addition
    Updated code to account for padding

--- BMP_Handler32d/BMP_Handler32d/BMP_Handler32d.cpp
+++ BMP_Handler32d/BMP_Handler32d/BMP_Handler32d.cpp
@@ -15,8 +15,36 @@ data.
 
 
 
-BITMAPFILEHEADER bmpHeader;
-BITMAPINFOHEADER bmpInfo; 
+//BITMAPFILEHEADER bmpHeader1;
+//BITMAPINFOHEADER bmpInfo; 
+
+typedef struct tag_bitmapFile 
+{
+        WORD    bfType;
+		DWORD   bfSize;
+        WORD    bfReserved1;
+        WORD    bfReserved2;
+        DWORD   bfOffBits;
+} bitmapFile;
+
+
+typedef struct tag_bitmapInfo
+{
+		DWORD      biSize;
+        LONG       biWidth;
+        LONG       biHeight;
+        WORD       biPlanes;
+        WORD       biBitCount;
+        DWORD      biCompression;
+        DWORD      biSizeImage;
+        LONG       biXPelsPerMeter;
+        LONG       biYPelsPerMeter;
+        DWORD      biClrUsed;
+        DWORD      biClrImportant;
+} bitmapInfo;
+
+bitmapFile bmpHeader;
+bitmapInfo bmpInfo; 
 
 BMP_API unsigned char* BMP_Handler::loadBMP(const char* filename, int& width, int& height)
 {	
@@ -30,8 +58,8 @@ BMP_API unsigned char* BMP_Handler::loadBMP(const char* filename, int& width, in
 	}
 
 	//Reads in the bitmap headers
-	file.read((char*)&bmpHeader, sizeof(BITMAPFILEHEADER));
-	file.read((char*)&bmpInfo, sizeof(BITMAPINFOHEADER));
+	file.read((char*)&bmpHeader, sizeof(bitmapFile) - 2);
+	file.read((char*)&bmpInfo, sizeof(bitmapInfo) - 2);
 
 	// Check if the file is an actual BMP file
 	if(bmpHeader.bfType != 0x4D42)
@@ -45,10 +73,10 @@ BMP_API unsigned char* BMP_Handler::loadBMP(const char* filename, int& width, in
 	height = (int)bmpInfo.biHeight;
 
 	//Array that will store altered bits
-	unsigned char* pixels = new unsigned char[bmpHeader.bfSize];
+	unsigned char* pixels = new unsigned char[width*height*4];
 
 	//Reads in RGB values to the pixels array
-	file.read((char*)pixels, bmpHeader.bfSize);
+	file.read((char*)pixels, width*height*4);
 
 	file.close();
 	return pixels;
@@ -59,11 +87,11 @@ BMP_API void BMP_Handler::saveBMP(const char* filename, const unsigned char* RGB
 	std::ofstream myfile(filename, std::ios::out | std::ios::binary | std::ofstream::trunc);
 
 	//Moves through headers
-	myfile.write((const char*)&bmpHeader, sizeof(BITMAPFILEHEADER));
-	myfile.write((const char*)&bmpInfo, sizeof(BITMAPINFOHEADER));
+	myfile.write((const char*)&bmpHeader, sizeof(bitmapFile) - 2);
+	myfile.write((const char*)&bmpInfo, sizeof(bitmapInfo) - 2);
 
 	//Writes out altered bits to image
-	myfile.write((const char*)RGB_values, 3*width*height);
+	myfile.write((const char*)RGB_values, 4*width*height);
 
 	myfile.close();
 }
